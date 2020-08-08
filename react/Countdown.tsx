28170b9ff0beab7b583ea76a67632c73abd7b883
renatopcinfo <renatopcinfo@gmail.com>
2020-08-08 20:20:24 -0300

    link app and thme

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
-}
+  return (
+     <div>
+     <h1>Teste Countdown</h1>
+     </div>
+       )
+    }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
